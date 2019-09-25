6ade4cf0779d21ec381e3aa681bb96f928923290
Sone <sonehuusone@gmail.com>
2019-09-25 15:22:34 +0700

    Restructure code and add makefile

---
+++ exercise2/math.c
@@ -0,0 +1,6 @@
+#include "math.h"
+
+void tong_hieu(int a, int b, float c, float d){
+    printf("%d %d\n", a+b,a-b);
+    printf("%.1f %.1f", c+d,c-d);
+}
---
+++ exercise2/math.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void tong_hieu(int a, int b, float c, float d);
